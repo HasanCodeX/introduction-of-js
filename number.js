85338ba2f395d8ae301c1302e2bce4a6c5cdb9f5
HasanCodeX <dev.hasan10@gmail.com>
2025-02-08 00:24:31 +0600

    shorthand.js prctise

---
+++ number.js
@@ -0,0 +1,17 @@
+var orangPrice = 20;
+var applePrice = 30;
+
+console.log(orangPrice);
+console.log(applePrice);
+console.log(typeof orangPrice + applePrice
+
+
+
+
+
+    
+);
+console.log(typeof applePrice);
+console.log(typeof 22);
+console.log(typeof 22);
+console.log(typeof 22);
---
+++ shorthand.js
@@ -0,0 +1,4 @@
+var price = 35 ;
+price = price + 5 ;
+price = price += 5 ;
+console.log(price); 
