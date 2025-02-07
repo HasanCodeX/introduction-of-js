289a2860fd9df0baea10fd909584ed2e72f06604
HasanCodeX <dev.hasan10@gmail.com>
2025-02-07 23:12:31 +0600

    type.js

---
+++ math.js
@@ -0,0 +1,14 @@
+// mathe needed for porgraming
+
+//1. number 
+//2.integer
+//3.float
+//4.Oparation
+//5.More oparation
+//.6.parcentege
+//7. sum
+//8. avarage
+//9. min/max
+//10.series
+//11.area tranguler
+//12.
---
+++ type.js
@@ -0,0 +1,14 @@
+var name = 'shantona dey'
+console.log(name);
+
+var address = "indian";
+var Country = "India";
+console.log(Country);
+
+var isRich = true;  
+console.log(isRich);
+
+var passed = false;
+console.log(passed);
+console.log(typeof passed);
+console.log(typeof isRich);
